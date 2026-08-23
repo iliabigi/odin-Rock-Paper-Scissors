fee38028a1804db31cc4c98e1640c69ca66bcfd6
iliabigi <238506978+iliabigi@users.noreply.github.com>
2026-08-23 14:33:14 +0330

    Add functions for human choice & computer choice

---
+++ script.js
@@ -0,0 +1,26 @@
+function getComputerChoice() {
+    let pick = Math.floor(Math.random() * 3) + 1; //1 = rock, 2 = paper, 3 = scissors.
+
+    if (pick === 1) {
+        return "rock";
+    } else if (pick === 2) {
+        return "paper";
+    } else if (pick === 3) {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let pick = prompt("ROCK? PAPER? SCISSORS?");
+    pick = pick.toLowerCase();
+
+    if (pick === "rock") {
+        return "rock";
+    } else if (pick === "paper") {
+        return "paper";
+    } else if (pick === "scissors") {
+        return "scissors";
+    } else {
+        console.log("You have exited the game.");
+    }
+}
